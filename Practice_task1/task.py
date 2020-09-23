41d00a64adbd08af538777eda599e1792fbeacb8
ValeriaMahdenko <70704714+ValeriaMahdenko@users.noreply.github.com>
2020-09-23 22:32:57 +0300

    Update task.py

--- Practice_task1/task.py
+++ Practice_task1/task.py
@@ -1,4 +1,10 @@
-def task(size):
+def appropriation(arr):
+    for j in range(1, len(arr)):
+        arr[j] += arr[j - 1]
+    return arr
+
+
+def finding_ways(size):
     # The function for finding ways.
     arr = [1 for i in range(size)]
     for i in range(size - 1):
@@ -6,29 +12,29 @@ def task(size):
         for j in range(len(arr)):
             narr[j] += arr[j]
             narr[j + 1] += arr[j]
-
         arr = narr
-        for j in range(1, len(arr)):
-            arr[j] += arr[j - 1]
-
+        appropriation(arr)
     for i in range(size - 1):
         narr = [0 for x in range(len(arr) - 1)]
-        for j in range(len(arr) - 1):
-            narr[j] += arr[j]
-            narr[j] += arr[j + 1]
-
+        for k in range(len(arr) - 1):
+            narr[k] += arr[k]
+            narr[k] += arr[k + 1]
         arr = narr
-        for j in range(1, len(arr)):
-            arr[j] += arr[j - 1]
+        appropriation(arr)
     return arr[-1]
 
 
-try:
-    n = int(input("Enter n: "))
-    if n < 0:
-        print("Number must be positive!")
+def validate_size():
+    try:
+        n = int(input("Enter n: "))
+        if n < 0:
+            print("Number must be positive!")
+            exit(0)
+    except ValueError:
+        print("Number must be an integer!")
         exit(0)
-except ValueError:
-    print("Number must be an integer!")
-    exit(0)
-print("Number of ways:  ", task(n))
+    return n
+
+
+n = validate_size()
+print("Number of ways:  ", finding_ways(n))
