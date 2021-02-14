58c5f8edcfda575839963ddae41eb6a6085ff1a0
ValeriaMahdenko <70704714+ValeriaMahdenko@users.noreply.github.com>
2021-02-14 17:06:47 +0200

    Create User_Table.py

---
+++ Python/programming_10/models/User_Table.py
@@ -0,0 +1,73 @@
+from validation import Validation
+from app.app import db
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+from enum import Enum
+
+class Role(Enum):
+    Admin = 1
+    User = 2
+
+class TableUser(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    _first_name = db.Column(db.String(20), unique=False)
+    _second_name = db.Column(db.String(20), unique=False)
+    _email = db.Column(db.String(100), unique=False)
+    _password = db.Column(db.String(1000), unique=False)
+    _role = db.Column(db.Enum('Admin', 'User'), unique=False)
+
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    @Validation.decorator_string
+    def first_name(self, val):
+        self._first_name = val
+
+    @property
+    def second_name(self):
+        return self._second_name
+
+    @second_name.setter
+    @Validation.decorator_string
+    def second_name(self, val):
+        self._second_name = val
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, val):
+        self._password = generate_password_hash(val, method='sha256')
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    @Validation.decorator_email
+    def email(self, val):
+        self._email = val
+
+    @property
+    def role(self):
+        return self._role
+
+    @role.setter
+    def role(self, val):
+        self._role = val
+
+
+    def to_json(self):
+        d = {}
+        d['user_id'] = self.user_id
+        d['first_name'] = self.first_name
+        d['second_name'] = self.second_name
+        d['email'] = self.email
+        d['password'] = self.password
+        d['role'] = self.role
+        return d
+
