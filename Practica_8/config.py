51b621c99046dd58cdd32e23f5fca0f2de42ae65
ValeriaMahdenko <70704714+ValeriaMahdenko@users.noreply.github.com>
2021-02-14 17:09:23 +0200

    Delete config.py

--- Practica_8/config.py
+++
@@ -1,6 +0,0 @@
-import pymysql
-DB_URI = "mysql+pymysql://{user}:{password}@{host}:{port}/{db}".format(user='root',
-                                                                       password='12345678',
-                                                                       host="127.0.0.1",
-                                                                       port="3306",
-                                                                       db="department")
