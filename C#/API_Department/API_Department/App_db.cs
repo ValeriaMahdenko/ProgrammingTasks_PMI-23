35714890eddb8cfed6046214fe13b7c95955a1a7
ValeriaMahdenko <70704714+ValeriaMahdenko@users.noreply.github.com>
2021-03-15 13:18:09 +0200

    Create App_db.cs

---
+++ C#/API_Department/API_Department/App_db.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace API_Department
+{
+    public class App_db : IDisposable
+    {
+        public MySqlConnection Connection { get; }
+
+        public App_db(string connectionString)
+        {
+            Connection = new MySqlConnection(connectionString);
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}
