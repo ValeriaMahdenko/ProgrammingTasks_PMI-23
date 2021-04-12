a8f978f0ad062d641f111c1dbc19c30b445b0e51
ValeriaMahdenko <vumahdenko6@gmail.com>
2021-04-12 17:55:59 +0300

    Create Context.cs

---
+++ C#/API_Department_orders/API_Department_orders/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace API_Department_orders.Models
+{
+    public class Context :DbContext
+    {
+        public Context() { }
+       
+        public Context(DbContextOptions<Context> options) : base(options) { }
+         
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Person> Users { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
