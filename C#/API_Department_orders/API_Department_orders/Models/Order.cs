6038e55b0b700c4b74e72843bacf7c82f8613a69
ValeriaMahdenko <vumahdenko6@gmail.com>
2021-04-12 17:55:50 +0300

    Create Order.cs

---
+++ C#/API_Department_orders/API_Department_orders/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API_Department_orders.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Range(1, 1000, ErrorMessage = "Amount must be 1-1000")]
+        public int Amount { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+       
+
+    }
+}
